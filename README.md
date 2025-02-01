362a13f12c8897de9e318d952c0e798230a6d198
Andrew Njaya <andrew.njaya@savannahinformatics.com>
2025-02-01 17:17:22 +0300

    feat: Add home endpoint and an catch invalid resource request

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# simple-rust-single-thread-web-app
+# Rust single thread web app
 A very simple rust web app that processes requests one at a time
+
+
+## Language
+```
+Rust
+```
+
+## Views
+### Home
+```
+GET 127.0.0.1:7878/
+```
+
+### Invalid resource
+```
+GET 127.0.0.1:7878/invalid_resource
+```
+
+### Run
+```
+Cargo run
+```
+
+### Simulate one thread
+Uncomment the code bellow with `match` and comment on `if/else` block. When the `/sleep` new resource is requested, other requests will have to wait.
+#### Uncomment
+```rust
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        },
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+```
+#### comment
+```rust
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1"{
+
+        // print!("Request {http_request:#?}");
+        // let response = "HTTP/1.1 200 OK\r\n\r\n";
+        // let status_line = "HTTP/1.1 200 OK";
+        // let contents = fs::read_to_string("hello.html").unwrap();
+        // let length = contents.len();
+
+        // let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+        // stream.write_all(response.as_bytes()).unwrap();
+        ("HTTP/1.1 200 OK", "hello.html")
+    }
+    else {
+        // some code
+        // let status_line = "HTTP/1.1 404 NOT FOUND";
+        // let contents = fs::read_to_string("404.html").unwrap();
+        // let length = contents.len();
+
+        // let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        // stream.write_all(response.as_bytes()).unwrap();
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    }
+```
+
+
---
+++ src/main.rs
@@ -0,0 +1,71 @@
+use std::{
+    fs,
+    io::{prelude::*, BufRead, BufReader},
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration
+};
+
+
+fn main(){
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming(){
+        let stream = stream.unwrap();
+
+        // println!("Connection established!")
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream){
+    let buf_reader = BufReader::new(&stream);
+    // let http_request: Vec<_> = buf_reader
+    //     .lines()
+    //     .map(|result| result.unwrap())
+    //     .take_while(|line| !line.is_empty())
+    //     .collect();
+
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1"{
+
+        // print!("Request {http_request:#?}");
+        // let response = "HTTP/1.1 200 OK\r\n\r\n";
+        // let status_line = "HTTP/1.1 200 OK";
+        // let contents = fs::read_to_string("hello.html").unwrap();
+        // let length = contents.len();
+
+        // let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+        // stream.write_all(response.as_bytes()).unwrap();
+        ("HTTP/1.1 200 OK", "hello.html")
+    }
+    else {
+        // some code
+        // let status_line = "HTTP/1.1 404 NOT FOUND";
+        // let contents = fs::read_to_string("404.html").unwrap();
+        // let length = contents.len();
+
+        // let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        // stream.write_all(response.as_bytes()).unwrap();
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+
+    };
+
+    // let (status_line, filename) = match &request_line[..] {
+    //     "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+    //     "GET /sleep HTTP/1.1" => {
+    //         thread::sleep(Duration::from_secs(5));
+    //         ("HTTP/1.1 200 OK", "hello.html")
+    //     },
+    //     _ => ("HTTP/1.1 404 NOT FOUND", "404.html")
+    // };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
+}
